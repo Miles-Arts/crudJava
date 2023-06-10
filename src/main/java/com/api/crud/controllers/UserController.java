be731106afdfa78ad439bf1f7d0078f83e52f53b
Milton Figueredo Miles <gratno@gmail.com>
2023-06-10 17:30:35 -0500

    @Get and @Post

--- src/main/java/com/api/crud/controllers/UserController.java
+++ src/main/java/com/api/crud/controllers/UserController.java
@@ -1,18 +1,37 @@
 package com.api.crud.controllers;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.api.crud.models.UserModel;
+import com.api.crud.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
 
     @GetMapping  /*("/getusers")*/
+    public ArrayList<UserModel>  getUsers() {
 
+        return this.userService.getUsers();
 
+    }
+
+    @PostMapping
+    public UserModel saveUser(@RequestBody UserModel user) {
+        return this.userService.saveUser(user);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<UserModel> getUserById(@PathVariable long id) {
+        return this.userService.getById(id);
+    }
 
 
 }
--- src/main/java/com/api/crud/services/UserService.java
+++ src/main/java/com/api/crud/services/UserService.java
@@ -5,7 +5,9 @@ import com.api.crud.repositories.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -18,7 +20,16 @@ public class UserService {
 
         return (ArrayList<UserModel>) userRepository.findAll();
 
+    }
 
+    public UserModel saveUser(UserModel user) {
+
+        return userRepository.save(user);
+
+    }
+
+    public Optional<UserModel> getById(Long id) {
+        return userRepository.findById(id);
     }
 
 }
