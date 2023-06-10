dbecf309ce90143f93d87bd05ddac042c56119ae
Milton Figueredo Miles <gratno@gmail.com>
2023-06-10 16:40:05 -0500

    Create @anotations and UserService and UserModel

--- src/main/java/com/api/crud/controllers/UserController.java
+++ src/main/java/com/api/crud/controllers/UserController.java
@@ -1,4 +1,18 @@
 package com.api.crud.controllers;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+
+    @GetMapping  /*("/getusers")*/
+
+
+
+
 }
